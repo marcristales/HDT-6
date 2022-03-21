a540cd64d60fe89a483c791ada751cd57f978561
Sly <cri21631@uvg.edu.gt>
2022-03-21 15:41:14 -0600

    Update HashMap

--- src/HashMap.java
+++ src/HashMap.java
@@ -12,31 +12,65 @@ public class HashMap<K, V> implements IMap<K, V> {
     	this.bucket = new MapBucket[CAPACITY];
     }
     
+    private int getHash(K key) {
+        return (key.hashCode() & 0xfffffff) % CAPACITY;
+    }
+    
+    private Entry getEntry(K key) {
+        int hash = getHash(key);
+        for (int i = 0; i < bucket[hash].getEntries().size(); i++) {
+            Entry entry = bucket[hash].getEntries().get(i);
+            if (entry.getKey().equals(key)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+    
 	@Override
 	public void put(K key, V value) {
-		// TODO Auto-generated method stub
+		
+		if(containsKey(key)) {
+            Entry entry = getEntry(key);
+            entry.setValue(value);
+        } else {
+            int hash = getHash(key);
+            if(bucket[hash] == null) {
+                bucket[hash] = new MapBucket();
+            }
+            bucket[hash].addEntry(new Entry<>(key, value));
+            size++;
+        }
+		
 		
 	}
 
 	@Override
 	public V get(K key) {
-		return null;
+		return containsKey(key) ? (V) getEntry(key).getValue() : null;
 	}
 
+	public boolean containsKey(K key) {
+        int hash = getHash(key);
+        return !(Objects.isNull(bucket[hash]) || Objects.isNull(getEntry(key)));
+    }
+	
 	@Override
 	public void delete(K key) {
-		// TODO Auto-generated method stub
+		
+		if(containsKey(key)) {
+            int hash = getHash(key);
+            bucket[hash].removeEntry(getEntry(key));
+            size--;
+        }
 		
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
-	
-	
 }
 
 class MapBucket {
